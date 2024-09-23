b71e97ee0e981f2b5b3a68f217749fc6352438df
Hyper1247 <95578590+Hyper1247@users.noreply.github.com>
2024-09-23 14:17:31 +0530

    Update app.py

--- Cryptocoin_Future_Price_Prediciton-main/app.py
+++ Cryptocoin_Future_Price_Prediciton-main/app.py
@@ -1,3 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
